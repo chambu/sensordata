1c1c84cded73bf87ff6f679dbd6e162ba8f657b3
chambu <gowthamj@gmail.com>
2015-08-20 14:05:55 -0400

    Delete SparkWebServer.js

--- SparkWebServer.js
+++
@@ -1,5 +0,0 @@
-var Spark = require('spark');
-
-Spark.login({ username: 'gowthamj@gmail.com', password: 'g0wth@m77' }, function(err, body) {
-  console.log('API call login completed on callback:', body);
-});
